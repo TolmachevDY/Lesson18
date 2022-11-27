74b4abb4c7834980f6413faaee857b92b5a39c42
Tolmachev DY <Tolmachev DY@DESKTOP-5RJHKAS>
2022-11-27 21:07:46 +0300

    Добавьте файлы проекта.

---
+++ lab18/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COLLECTIONS
+{    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            Console.WriteLine(Check(str));
+            Console.ReadKey();
+        }
+
+        static bool Check(string str)//Создаем метод для проверки условий
+        {            
+            Stack<char> stack = new Stack<char>();           
+            Dictionary<char, char> sk = new Dictionary<char, char>()
+            {
+                {'(',')' },
+                {'{','}' },
+                {'[',']' },
+            };
+            //Перебираем посимвольно строку
+            foreach (char c in str)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                    stack.Push(sk[c]);
+                if (c == ')' || c == '}' || c == ']')
+                {
+                    if (stack.Count == 0 || stack.Pop() != c)
+                        return false;
+                }
+            }            
+            if (stack.Count == 0)
+                return true;
+            else
+                return false;//количество открытых скобок превышает количество закрытых
+        }
+    }
+}
---
+++ lab18/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lab18")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab18")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("23a3197a-27b7-47e1-b5c2-2ab18464a355")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
